3454db6e00dc41d671c9b37ec213eac94ddf118b
Patrick Niyo <patrickniyogitare28@gmail.com>
2025-03-28 00:22:07 +0200

    fix types

--- src/app/(dashboard)/dashboard/settings/page.tsx
+++ src/app/(dashboard)/dashboard/settings/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 "use client"
 import React, { useState } from 'react';
 import { 
