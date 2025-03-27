05f0291ef4be2804e05e0648fdf2981e04aef28e
Patrick Niyo <patrickniyogitare28@gmail.com>
2025-03-27 23:31:08 +0200

    ft(dashboard): enhanced the sidebar functionality

---
+++ src/app/(dashboard)/dashboard/mentors/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardMentors() {
+    return (
+        <div>
+            <h1>Mentors Page</h1>
+        </div>
+    );
+}
---
+++ src/app/(dashboard)/dashboard/messages/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardMessages() {
+    return (
+        <div>
+            <h1>Messages Page</h1>
+        </div>
+    );
+}
